4d4c61baca33bcf7545c4e17d65389b1bf1bcf2d
Jessyca Costa <jecosta@c1r6s5.42porto.com>
2024-12-09 10:17:10 +0000

    added .gdbinit

--- nvim/lua/zedro/core/global.lua
+++ nvim/lua/zedro/core/global.lua
@@ -15,8 +15,8 @@ vim.g.node_host_prog = '~/.nvm/versions/node/v20.11.1/bin/node'
 vim.cmd("let $PATH = '/path/to/node/bin:' . $PATH")
 
 -- 42 Header Settings
-vim.g.user42 = 'passunca'
-vim.g.mail42 = 'passunca@student.42porto.com'
+vim.g.user42 = 'jecosta'
+vim.g.mail42 = 'jecosta@student.42porto.com'
 
 -- Rest.nvim
 vim.g.rest_nvim = {
